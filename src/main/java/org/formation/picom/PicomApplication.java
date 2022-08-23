892679b3e0090aa68629fad5c869533c0a1bb962
Thibaut <tibeloute@msn.com>
2022-08-23 11:30:17 +0200

    first commit

---
+++ src/main/java/org/formation/picom/PicomApplication.java
@@ -0,0 +1,13 @@
+package org.formation.picom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/formation/picom/PicomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.picom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
