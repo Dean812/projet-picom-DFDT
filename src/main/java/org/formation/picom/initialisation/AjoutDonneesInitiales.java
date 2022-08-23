5f61b32119abbb688762b5c5b99b7ad3d3b6b646
Florian Demette <florian.a.demette@gmail.com>
2022-08-23 19:24:33 +0200

    Ajout du package et de la classe initialisation

---
+++ src/main/java/org/formation/picom/initialisation/AjoutDonneesInitiales.java
@@ -0,0 +1,18 @@
+package org.formation.picom.initialisation;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class AjoutDonneesInitiales implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
