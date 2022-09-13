39e56c2c44c10d0de1e67fd280012d66afa57782
Thibaut <tibeloute@msn.com>
2022-09-13 19:43:49 +0200

    gestion Login/logoff - redirection client et Admin  - gestion avec angular

--- src/main/java/org/formation/picom/controller/rest/UtilisateurRestController.java
+++ src/main/java/org/formation/picom/controller/rest/UtilisateurRestController.java
@@ -1,9 +1,12 @@
 package org.formation.picom.controller.rest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.formation.picom.business.Administrateur;
 import org.formation.picom.business.Client;
 import org.formation.picom.business.Utilisateur;
 import org.formation.picom.dto.ClientDto;
@@ -19,12 +22,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @RequestMapping("api/utilisateurs/")
 public class UtilisateurRestController {
 
 	@Autowired
 	UtilisateurService utilisateurService;
+	ObjectMapper om = new ObjectMapper();     // loggin phase 2
 
 	@GetMapping("ensembleUtilisateurs")
 	public List<Utilisateur> recupererUtilisateurs() {
@@ -46,8 +52,18 @@ public class UtilisateurRestController {
 
 	@PostMapping("authUtilisateur")
 	@ResponseStatus(code = HttpStatus.OK)
-	public Utilisateur recupererUtilisateur(@RequestBody ClientDto clientDto) throws Exception { // if nok > renvoie une (dirty) exception
-		return utilisateurService.recupererUtilisateur(clientDto.getEmail(), clientDto.getMotDePasse());
+	public String recupererUtilisateur(@RequestBody ClientDto clientDto) throws Exception {   // suite au passage a >> writeValueAsString , remplacement de utilisateur par string
+		Utilisateur utilisateurLog = utilisateurService.recupererUtilisateur(clientDto.getEmail(), clientDto.getMotDePasse()); // gestion log - coté log first 
+		System.out.println(utilisateurLog instanceof Administrateur ); // vérifie si l'instance existe ( boolean )  - until , on identifie par true or false si admin or not ( only back/java) 
+		Map<String,Object> logMap = new HashMap<>(); 
+		if(utilisateurLog instanceof Administrateur) {
+			logMap.put("utilisateur",utilisateurLog);
+			logMap.put("role","roleAdmin");
+		}else if(utilisateurLog instanceof Client){
+			logMap.put("utilisateur",utilisateurLog);
+			logMap.put("role","roleClient");
+		}
+		return om.writeValueAsString(logMap); // erreur , on nous demande un string, du coup > writeValueAsString, retourne un Json de la map ( qui est un string ) 
 	}
 
 }
